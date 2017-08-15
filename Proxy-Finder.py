628dcd7be5c3cdc79fd2c7af48f3ec3b9c9e1415
Lee Hayes <leehayes@gmail.com>
2017-08-15 17:01:01 +0100

    Built out proxy decorator

--- Proxy-Finder.py
+++ Proxy-Finder.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.5
+from functools import wraps
 
 '''Finds elite anonymity (L1) HTTP proxies asyncronously.
 '''
@@ -10,5 +10,39 @@ __author__ = 'Lee Hayes'
 __contact__ = '@leehayes81'
 
 
-import sys, re, time, os, argparse
-from BeautifulSoup4 import BeautifulSoup
+def proxy_source(method):
+    @wraps
+    def wrapper(self, *args, **kwargs):
+        print("dfasd")
+        self.proxy_sourcing_functions.append(method)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+class ProxyFinder(object):
+    '''
+    The primary class responsible for sourcing, storing and presenting proxy ips
+    '''
+
+    def __init__(self):
+        self.proxy_sourcing_functions = []
+        pass
+
+    def all_sources(self):
+        for func in self.proxy_sourcing_functions:
+            print(func)
+
+    @proxy_source
+    def get_proxy_from_sitename(self, ):
+        '''
+        Return proxy ips for .....
+        :return: a list of dictionaries detailing proxy details
+        '''
+        return 45
+
+
+if __name__ == "__main__":
+    pf = ProxyFinder()
+    pf.all_sources()
+    # pf.get_proxy_from_sitename
