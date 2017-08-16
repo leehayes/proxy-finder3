cc11c21bb7b48f3f9a86c96af17f1af538e4d250
Lee Hayes <leehayes@gmail.com>
2017-08-16 11:36:37 +0100

    Completed source for gimmeproxy.com

--- Proxy-Finder.py
+++ Proxy-Finder.py
@@ -1,4 +1,8 @@
-from functools import wraps
+import asyncio
+import json
+from pprint import pprint
+
+import aiohttp
 
 '''Finds elite anonymity (L1) HTTP proxies asyncronously.
 '''
@@ -10,39 +14,64 @@ __author__ = 'Lee Hayes'
 __contact__ = '@leehayes81'
 
 
-def proxy_source(method):
-    @wraps
-    def wrapper(self, *args, **kwargs):
-        print("dfasd")
-        self.proxy_sourcing_functions.append(method)
-        return method(self, *args, **kwargs)
-
-    return wrapper
-
-
 class ProxyFinder(object):
     '''
     The primary class responsible for sourcing, storing and presenting proxy ips
     '''
 
-    def __init__(self):
-        self.proxy_sourcing_functions = []
-        pass
+    def __init__(self, gimme=1, othersite=0):
+        self.gimme = gimme
+        self.task_list_of_proxy_sourcing_functions = []
+        self.list_of_proxies = self.proxy_details
 
-    def all_sources(self):
-        for func in self.proxy_sourcing_functions:
-            print(func)
+    async def get_page_json(self, url):
+        async with aiohttp.ClientSession() as session:
+            response = await session.request('GET', url)
+            body = await response.read()
+            body = body.decode("utf-8")
+            json_res = json.loads(body)
+            return json_res
 
-    @proxy_source
-    def get_proxy_from_sitename(self, ):
+    ########################
+
+    async def view_gimmeproxy(self):
+        json = await self.get_page_json('http://gimmeproxy.com/api/getProxy')
+        return json
+
+    async def create_proxy_dict_gimmeproxy(self):
+        json = await self.view_gimmeproxy()
+        dict = json
+        return {'ip': dict.get('ip'), 'port': dict.get('port'), 'source': 'http://gimmeproxy.com/api/getProxy'}
+        # return dict.keys()
+        # 'supportsHttps', 'protocol', 'ip', 'port', 'get', 'post', 'cookies', 'referer', 'user-agent', 'anonymityLevel', 'websites', 'country', 'tsChecked', 'curl', 'ipPort', 'type', 'speed', 'otherProtocols']
+
+    ########################
+
+
+    ########################
+
+    @property
+    def proxy_details(self):
         '''
         Return proxy ips for .....
         :return: a list of dictionaries detailing proxy details
         '''
-        return 45
+
+        # Add Gimme tasks to task list
+        for i in range(self.gimme):
+            task = asyncio.ensure_future(self.create_proxy_dict_gimmeproxy())
+            self.task_list_of_proxy_sourcing_functions.append(task)
+
+        loop = asyncio.get_event_loop()
+        done, _ = loop.run_until_complete(asyncio.wait(self.task_list_of_proxy_sourcing_functions))
+        results = []
+        for fut in done:
+            results.append(fut.result())
+        loop.close()
+        return results
+
 
 
 if __name__ == "__main__":
-    pf = ProxyFinder()
-    pf.all_sources()
-    # pf.get_proxy_from_sitename
+    pf = ProxyFinder(gimme=2)
+    pprint(pf.list_of_proxies)
