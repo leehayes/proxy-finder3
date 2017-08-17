58ecea42c71d8e0da402dece6cc713b7f5bfbd09
Lee Hayes <leehayes@gmail.com>
2017-08-17 22:17:42 +0100

    Built tests

---
+++ test/unit/test_proxyFinder.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+
+class TestProxyFinder(TestCase):
+    def test_get_page_json(self):
+        self.fail()
+
+    def test_get_page_html(self):
+        self.fail()
+
+    def test_view_gimmeproxy(self):
+        self.fail()
+
+    def test_create_proxy_dict_gimmeproxy(self):
+        self.fail()
+
+    def test_view_freeproxylist_uk(self):
+        self.fail()
+
+    def test_create_proxy_dict_freeproxylist_uk(self):
+        self.fail()
+
+    def test_view_freeproxylist_us(self):
+        self.fail()
+
+    def test_create_proxy_dict_freeproxylist_us(self):
+        self.fail()
+
+    def test_view_gatherproxy(self):
+        self.fail()
+
+    def test_create_proxy_dict_gatherproxy(self):
+        self.fail()
+
+    def test_proxy_details(self):
+        self.fail()
+
+    def test_refresh(self):
+        self.fail()
